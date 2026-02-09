9982349214fa4aa20ef31e2e31fb57bd6811c70e
Abraham Galue <abrahamgalue512@gmail.com>
2026-02-09 17:32:27 -0400

    Solve case 0 from the grocery list

--- proyectos-live/lista-supermercado/caso-0/src/App.tsx
+++ proyectos-live/lista-supermercado/caso-0/src/App.tsx
@@ -6,23 +6,41 @@ import styles from "./App.module.scss";
 import api from "./api";
 
 function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    api.list().then(setItems);
+    api
+      .list()
+      .then(setItems)
+      .finally(() => setIsLoading(false));
   }, []);
+
+  function handleDelete(id: Item["id"]) {
+    const newItems = items.filter((item) => item.id !== id);
+
+    setItems(newItems);
+  }
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (items.length === 0) {
+    return <p>No hay elementos en la lista</p>;
+  }
 
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input autoFocus name="text" type="text" />
         <button>Add</button>
       </form>
       <ul>
         {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+          <li key={item.id} className={item.completed ? styles.completed : ""}>
+            {item.text} <button onClick={() => handleDelete(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
