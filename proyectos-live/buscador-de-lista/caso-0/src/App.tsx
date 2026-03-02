0522a7f97a8bcc707c104e74e50eccb98c81c332
Abraham Galue <abrahamgalue512@gmail.com>
2026-03-02 16:30:50 -0400

    Solve case 0 for the list finder

--- proyectos-live/buscador-de-lista/caso-0/src/App.tsx
+++ proyectos-live/buscador-de-lista/caso-0/src/App.tsx
@@ -7,18 +7,30 @@ import api from "./api";
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    api.search(query);
+    api.search(query).then((products) => {
+      setProducts(products);
+      setIsLoading(false);
+    });
   }, [query]);
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (products.length === 0) {
+    return <p>No hay productos</p>;
+  }
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
+          <li key={product.id} className={product.price <= 100 ? "sale" : ""}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
             <span>$ {product.price}</span>
